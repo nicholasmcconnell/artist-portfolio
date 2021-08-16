36b483be23b32d4cffd6c056e785f8b2eb0cb7f2
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-08-16 12:01:28 -0400

    created container cmponent for styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,23 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from "./components/Navbar/Navbar";
 import "./App.css";
 import { Form } from "./components/Form/Form";
+// import Container from "./components/Container1/Container";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+
+        {/* <Container> */}
+
         <Switch>
           <Route exact path='/contact' >
             <Form />
           </Route>
 
         </Switch>
+        {/* </Container> */}
       </Router>
 
     </div>
---
+++ src/components/Container1/Container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Container1() {
+
+    // const style = {
+    //     width: '100%',
+    //     background: 'green'
+    // }
+
+    return (
+        < >
+
+        </>
+    )
+}
