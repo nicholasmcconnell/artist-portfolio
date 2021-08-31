4d9aff27fe5820394612923fe9b873eab79b45b8
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-08-31 14:19:53 -0400

    added image folder to public folder

--- src/components/Form/FormSucces.tsx
+++ src/components/Form/FormSucces.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function FormSucces() {
     return (
-        <div>
-
+        <div className="form-content-right">
+            <div className="form-success">
+                We have received your request!
+            </div>
+            <img src="img/img-3.svg" alt="success-image" className="form-img-2" />
         </div>
     )
 }
